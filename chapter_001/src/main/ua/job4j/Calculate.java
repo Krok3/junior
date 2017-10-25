de16dd7ea1563358d13c83583fe024ee9e28d662
Krok3 <Vladyslav.Frundin@gmail.com>
2017-10-25 23:20:27 +0300

    Добавить JavaDoc.[#3]

--- chapter_001/src/main/ua/job4j/Calculate.java
+++ chapter_001/src/main/ua/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ua.job4j;
 
+/**
+* Class Класс для вычисления результата арифметических операций +, -, *, :, ^.
+* @author vfrundin
+* @since 25.10.2017
+* @version 1
+*/ 
 public class Calculate {
+	
+	/**
+	* Конструктор, вывод строки в консоль.
+	* @param arg - arg
+	*/ 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
