63ab1174684ec74904d673fe556bac0f9e36e8dc
Krok3 <Vladyslav.Frundin@gmail.com>
2017-11-01 21:25:20 +0200

    Максимум из двух чисел.[#7]

---
+++ chapter_001/src/main/java/ua/job4j/max/Max.java
@@ -0,0 +1,22 @@
+package ua.job4j.max;
+
+/**
+ * Class Класс для для нахождения максимума из двух целых чисел.
+ * @author vfrundin
+ * @since 01.11.2017
+ * @version 1.0
+ */
+public class Max {
+
+    /**
+     * Method add.
+     * @param first 1st number.
+     * @param second 2nd number.
+     * @return int maxValue.
+     */
+    public int max(int first, int second) {
+        int maxValue;
+        maxValue = (first >= second) ? first : second;
+        return maxValue;
+    }
+}
